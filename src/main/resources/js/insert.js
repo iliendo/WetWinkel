b158e31de182d816bf682cc4cb07d78c6929887b
Marouane <mar.tourak@gmail.com>
2018-10-09 13:31:10 +0200

    Client form created

---
+++ src/main/resources/js/insert.js
@@ -0,0 +1,15 @@
+function myFunction() {
+
+    var initialen = document.getElementById("initialen").value;
+    var tussenvoegsel = document.getElementById("tussenvoegsel").value;
+    var achternaam = document.getElementById("achternaam").value;
+    var straatnaam = document.getElementById("straatnaam").value;
+    var postcode = document.getElementById("postcode").value;
+    var huisnummer = document.getElementById("huisnummer").value;
+    var toevoeging = document.getElementById("toevoeging").value;
+    var land = document.getElementById("land").value;
+    var telefoonnummer = document.getElementById("telefoonnummer").value;
+    var email = document.getElementById("email").value;
+
+       document.getElementById("demo").innerHTML = initialen, tussenvoegsel;
+}
