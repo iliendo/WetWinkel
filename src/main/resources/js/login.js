5fcc2b40701e495fb952d02b8b2668c8bf80c2fc
Remco <remcotimmerman1@gmail.com>
2018-10-09 14:43:09 +0200

    created login js

---
+++ src/main/resources/js/login.js
@@ -0,0 +1,19 @@
+var user = document.getElementById("email");
+var password = document.getElementById("password");
+
+document.getElementById("login-button").onclick = function () {
+    login(user, password);
+};
+
+function login(user, password) {
+    switch (user.value) {
+        case "admin@wetwinkel.nl":
+            if (password.value = "12345") {
+                window.open("Client.html", "_self")
+            } else {
+                console.log("password is wrong");
+            }
+        default:
+            console.log("email wrong")
+    }
+}
