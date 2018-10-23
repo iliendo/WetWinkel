143da528ab9c24c2bd3aaa1f4cf03d4f867afe37
Remco <remcotimmerman1@gmail.com>
2018-10-23 19:12:57 +0200

    changed gitignore

--- src/main/java/Wetwinkel/Database/Database.java
+++ src/main/java/Wetwinkel/Database/Database.java
@@ -8,7 +8,7 @@ import javax.ejb.Stateless;
 @javax.annotation.sql.DataSourceDefinition(
         name = "java:comp/env/jdbc/wetwinkelDb",
         className = "com.mysql.cj.jdbc.MysqlXADataSource",
-        url = "145.92.203.241",
+        url = "jdbc:mysql://145.92.203.241:3306/ztourakm0011",
         user = "tourakm",
         password = "FBJFf4sFjV3Wg3")
 @Stateless
