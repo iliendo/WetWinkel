b1e15dccc389889b8b6d353b77fedcb973dcfaf1
Remco <remcotimmerman1@gmail.com>
2018-10-26 12:51:32 +0200

    fixed login values, need to get the token now

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,8 +1,8 @@
-var user = document.getElementById("email");
+var email = document.getElementById("email");
 var password = document.getElementById("password");
 
 document.getElementById("login-button").onclick = function () {
-    login(user, password);
+    login(email.value, password.value);
 };
 
 password.addEventListener("keyup", function(event) {
@@ -15,20 +15,29 @@ password.addEventListener("keyup", function(event) {
     }
 });
 
-function login(user, password) {
-    console.log("henk is groot, henk is de grootste");
-    xhr = new XMLHttpRequest();
+function login(email, password) {
+    // xhr = new XMLHttpRequest();
     var url = "http://localhost:8080/wetwinkel_war/rest/login";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-            console.log(json);
-        } else {
-            console.log(xhr.status);
+    // xhr.open("POST", url, true);
+    // xhr.setRequestHeader("Content-type", "application/json");
+    // xhr.onreadystatechange = function () {
+    //     if (xhr.readyState === 4 && xhr.status === 200) {
+    //         var json = JSON.parse(xhr.responseText);
+    //         console.log(json);
+    //     } else {
+    //         console.log(xhr.status);
+    //     }
+    // };
+    var data = {'email':email,'wachtwoord':password};
+    // xhr.send(data);
+
+    console.log(JSON.stringify(data));
+    fetch(url,  {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
         }
-    };
-    var data = JSON.stringify({"email":user,"wachtwoord":password});
-    xhr.send(data);
+    }).then(function (value) { console.log(value.headers.values()) });
+
 }
