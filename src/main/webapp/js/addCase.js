24bf76b3bf60d1297849a8eae5ef21ecc9ea627b
Unknown <ilias.azagagh@gmail.com>
2018-11-17 19:24:42 +0100

    Client now also shows middle name

--- src/main/webapp/js/addCase.js
+++ src/main/webapp/js/addCase.js
@@ -1,40 +1,10 @@
 // Fill comboboxes when page is loaded
-clientDDL();
 werknemerDDL();
-jurisdictieDDL();
+clientDDL();
 
 document.getElementById("add_button").onclick = function () {
     myFunction();
 };
-
-/**
- * Fills the client dropdownlist with the client of the database
- */
-function clientDDL() {
-    let client = document.getElementById("client");
-    let url = "http://localhost:8080/wetwinkel_war/rest/case/clients";
-
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'authorization': 'bearer ' + localStorage.getItem("token")
-        }
-    }).then(function (response) {
-        return response.json();
-    }).then(function (value) {
-        console.log(value);
-        for (let i = 0; i < value.length; i++) {
-
-            let client = value[i].initialen + " " + value[i].achternaam + " (" + value[i].postcode
-                + ", " + value[i].huisnummer + ")";
-
-
-            let option = document.createElement("OPTION");
-            option.value = value[i].idClient;
-            client.add(option);
-        }
-    });
-}
 
 function werknemerDDL() {
     let werknemer = document.getElementsByClassName("werknemer");
@@ -58,11 +28,6 @@ function werknemerDDL() {
                 employee = value[i].naam + " " + value[i].tussenvoegsel + " " + value[i].achternaam;
             }
 
-
-            console.log(werknemer[0]);
-            console.log(werknemer[1]);
-
-
             for (let j = 0; j < werknemer.length; j++) {
                 let option = document.createElement("OPTION");
                 let txt = document.createTextNode(employee);
@@ -75,8 +40,11 @@ function werknemerDDL() {
     });
 }
 
-function jurisdictieDDL() {
-    let jurisdictie = document.getElementById("client");
+/**
+ * Fills the client dropdown list with the client of the database
+ */
+function clientDDL() {
+    let clienten = document.getElementById("client");
     let url = "http://localhost:8080/wetwinkel_war/rest/case/clients";
 
     fetch(url, {
@@ -88,19 +56,51 @@ function jurisdictieDDL() {
         return response.json();
     }).then(function (value) {
         for (let i = 0; i < value.length; i++) {
+            let client;
 
-            let client = value[i].initialen + " " + value[i].achternaam + " (" + value[i].postcode
-                + ", " + value[i].huisnummer + ")";
-
+            if (value[i].tussenvoegsel === undefined) {
+                client = value[i].initialen + " " + value[i].achternaam + " (" + value[i].postcode + ", "
+                    + value[i].huisnummer + ")";
+            } else {
+                client = value[i].initialen + " " + value[i].tussenvoegsel + " " + value[i].achternaam + " ("
+                    + value[i].postcode + ", " + value[i].huisnummer + ")";
+            }
 
             let option = document.createElement("OPTION");
             let txt = document.createTextNode(client);
             option.appendChild(txt);
             option.value = value[i].idClient;
-            jurisdictie.insertBefore(option, jurisdictie.lastChild);
+            clienten.add(option)
         }
     });
 }
+
+/**
+ * Fills the client dropdown list with the client of the database
+ */
+// function jusrisdcitieDDL() {
+//     let jurisdictie = document.getElementById("jurisdictie");
+//     let url = "http://localhost:8080/wetwinkel_war/rest/case/jurisdictie";
+//
+//     fetch(url, {
+//         method: 'GET'
+//     }).then(function (response) {
+//         return response.json();
+//     }).then(function (value) {
+//         for (let i = 0; i < value.length; i++) {
+//
+//             let client = value[i].initialen + " " + value[i].achternaam + " (" + value[i].postcode
+//                 + ", " + value[i].huisnummer + ")";
+//
+//
+//             let option = document.createElement("OPTION");
+//             let txt = document.createTextNode(client);
+//             option.appendChild(txt);
+//             option.value = value[i].idClient;
+//             client.insertBefore(option, client.lastChild);
+//         }
+//     });
+// }
 
 function myFunction() {
 
